e0b6c0dd19b6f4063321fe608e8f04431644fa88
d116626 <d116626@gmail.com>
2025-12-01 14:53:02 -0300

    chore: add more libs

--- src/config/env.py
+++ src/config/env.py
@@ -6,3 +6,11 @@ if os.path.exists("src/config/.env"):
     import dotenv
 
     dotenv.load_dotenv(dotenv_path="src/config/.env")
+
+
+GCP_SERVICE_ACCOUNT_CREDENTIALS = getenv_or_action(
+    "GCP_SERVICE_ACCOUNT_CREDENTIALS", action="raise"
+)
+GOOGLE_BIGQUERY_PAGE_SIZE = int(
+    getenv_or_action("GOOGLE_BIGQUERY_PAGE_SIZE", default="100")
+)
