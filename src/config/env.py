a378f0ed3a4e8635428e8eda65467b5b9f27326d
d116626 <d116626@gmail.com>
2025-12-01 15:17:05 -0300

    feat: api v1

--- src/config/env.py
+++ src/config/env.py
@@ -1,7 +1,7 @@
 from src.utils.infisical import getenv_or_action
 import os
 
-# if file .env exists, load it
+# if file .env exists, load it.
 if os.path.exists("src/config/.env"):
     import dotenv
 
